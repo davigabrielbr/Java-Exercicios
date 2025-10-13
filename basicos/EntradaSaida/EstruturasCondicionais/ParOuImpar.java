e9ef3b888338fa16eeff9f092ed065089d4894b8
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-10-13 12:51:23 -0300

    add: ParOuImpar (exercício de estruturas condicionais)
    Implementação do exercício ParOuImpar que lê um número inteiro do usuário e verifica se ele é par ou ímpar.
    Utiliza a estrutura condicional if/else
    e operador módulo (%) para determinar a classificação.

---
+++ basicos/EntradaSaida/EstruturasCondicionais/ParOuImpar.java
@@ -0,0 +1,20 @@
+package EstruturasCondicionais;
+
+import java.util.Scanner;
+
+public class ParOuImpar {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número inteiro: ");
+        int numero = scanner.nextInt();
+
+        if (numero % 2 == 0) {
+            System.out.println("O número " + numero + " é Par!");
+        } else {
+            System.out.println("O número " + numero + " é Impar!");
+        }
+
+        scanner.close();
+    }
+}
