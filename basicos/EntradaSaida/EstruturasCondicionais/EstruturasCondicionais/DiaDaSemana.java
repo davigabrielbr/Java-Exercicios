4e02c607f6e315448ad134d58c42d90cf0a7ce54
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-10-14 16:10:55 -0300

    Adiciona exercício DiaDaSemana usando estrutura switch
    Cria programa que lê um número de 1 a 7 e exibe o dia da semana correspondente.
    Utiliza estrutura condicional switch para determinar o dia e trata casos inválidos.

---
+++ basicos/EntradaSaida/EstruturasCondicionais/EstruturasCondicionais/DiaDaSemana.java
@@ -0,0 +1,40 @@
+package EstruturasCondicionais;
+
+import java.util.Scanner;
+
+public class DiaDaSemana {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+
+        System.out.print("Digite um número (1 a 7): ");
+        int numero = scanner.nextInt();
+
+        switch (numero) {
+            case 1:
+                System.out.println("Segunda-feira");
+                break;
+            case 2:
+                System.out.println("Terça-feira");
+                break;
+            case 3:
+                System.out.println("Quarta-feira");
+                break;
+            case 4:
+                System.out.println("Quinta-feira");
+                break;
+            case 5:
+                System.out.println("Sexta-feira");
+                break;
+            case 6:
+                System.out.println("Sábado");
+                break;
+            case 7:
+                System.out.println("Domingo");
+                break;
+            default:
+                System.out.println("Número inválido! Digite um valor entre 1 e 7.");
+        }
+
+        scanner.close();
+    }
+}
