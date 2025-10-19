8865b6e716ac6b4ec4ba15afce96a940a181057e
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-10-19 15:06:23 -0300

    Adiciona exercício CriandoLivro usando POO e Criando Classe
    Cria dois objetos da classe CriandoLivro e adiciona o método ler() para simular a leitura.
    Exibe o status completo de cada livro, mostrando título, autor, editora, total de páginas e se está aberto.
    Código organizado dentro do pacote POO.

---
+++ basicos/EntradaSaida/POO/CriandoLivro.java
@@ -0,0 +1,33 @@
+package POO;
+
+public class CriandoLivro {
+    String titulo;
+    String autor;
+    String editora;
+    int totalPaginas;
+    boolean aberto;
+
+    void status() {
+        System.out.println("Título: " + this.titulo);
+        System.out.println("Autor: " + this.autor);
+        System.out.println("Editora: " + this.editora);
+        System.out.println("Total de páginas: " + this.totalPaginas);
+        System.out.println("Está aberto: " + this.aberto);
+    }
+
+    void abrir() {
+        this.aberto = true;
+    }
+
+    void fechar() {
+        this.aberto = false;
+    }
+
+    void ler() {
+        if (this.aberto == true) {
+            System.out.println("Lendo o livro " + this.titulo + "...");
+        } else {
+            System.out.println("Abrir o livro " + this.titulo + " antes de ler!");
+        }
+    }
+}
---
+++ basicos/EntradaSaida/POO/Livro.java
@@ -0,0 +1,28 @@
+package POO;
+
+public class Livro {
+    public static void main(String[] args) {
+        CriandoLivro l1 = new CriandoLivro();
+        l1.titulo = "Nada pode me ferir";
+        l1.autor = "David Goggins";
+        l1.editora = "Editora Sextante";
+        l1.totalPaginas = 320;
+
+        System.out.println("Livro 1");
+        l1.abrir();
+        l1.status();
+        l1.ler();
+
+        CriandoLivro l2 = new CriandoLivro();
+        l2.titulo = "Hábitos Atômicos";
+        l2.autor = "James Clear";
+        l2.editora = "Alfa Life";
+        l2.totalPaginas = 320;
+
+        System.out.println("---------------------------------");
+        System.out.println("Livro 2");
+        l2.fechar();
+        l2.status();
+        l2.ler();
+    }
+}
