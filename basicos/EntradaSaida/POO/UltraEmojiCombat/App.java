6e4d7ca919a00b52b6c50e8147ee801dbe1552d9
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-11-25 10:51:07 -0300

    Agregação entre Objetos
    Fazendo relacionamento de agregação entre as classes.

---
+++ basicos/EntradaSaida/POO/UltraEmojiCombat/App.java
@@ -0,0 +1,25 @@
+package UltraEmojiCombat;
+
+public class App {
+    public static void main(String[] args) {
+        Lutador l[] = new Lutador[6];
+
+        l[0] = new Lutador("Pretty boy", "França", 31, (float) 1.75, (float) 68.9, 11, 2, 1);
+
+        l[1] = new Lutador("Putscript", "Brasil", 29, (float) 1.68, (float) 57.8, 14, 2, 3);
+
+        l[2] = new Lutador("Snapshadow", "EUA", 35, (float) 1.65, (float) 80.9,12, 2, 1);
+
+        l[3] = new Lutador("Dead Code", "Austrália", 28, (float) 1.93, (float) 81.6,13, 0, 2);
+
+        l[4] = new Lutador("Ufocobol", "Brasil", 37, (float) 1.70, (float) 119.3,5, 4, 3);
+
+        l[5] = new Lutador("Nerdaard", "EUA", 30, (float) 1.81, (float) 105.7,12, 2, 4);
+
+        Luta UEC01 = new Luta();
+        UEC01.marcarLuta(l[0], l[1]);
+        UEC01.lutar();
+        l[0].status();
+        l[1].status();
+    }
+}
