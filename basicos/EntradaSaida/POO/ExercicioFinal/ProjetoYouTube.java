712bb020b4f298c4f38e5f3b1c0ede077d3eff5e
Davi Gabriel <davi_gabriel99@hotmail.com>
2025-12-02 10:24:26 -0300

    Projeto final em Java

--- basicos/EntradaSaida/POO/ExercicioFinal/ProjetoYouTube.java
+++ basicos/EntradaSaida/POO/ExercicioFinal/ProjetoYouTube.java
@@ -11,7 +11,21 @@ public class ProjetoYouTube {
         g[0] = new Gafanhoto("Davi", 21, "M", "davi86154");
         g[1] = new Gafanhoto("Adrielly", 24, "F", "drika");
 
-        System.out.println(g[0].toString());
+        Visualizacao vis[] = new Visualizacao[5];
+        vis[0] = new Visualizacao(g[0], v[0]); // Davi assiste Projeto Final em Java
+        vis[0].avaliar();
+        System.out.println(vis[0].toString());
+        
+        vis[1] = new Visualizacao(g[0], v[1]); // Davi assiste Java POO
+        vis[1].avaliar(87.0f);
+        System.out.println(vis[1].toString());
+
+        /*System.out.println("VÍDEOS\n---------------------------------");
         System.out.println(v[0].toString());
+        System.out.println(v[1].toString());
+        System.out.println(v[2].toString());
+        System.out.println("PESSOAS\n---------------------------------");
+        System.out.println(g[0].toString());
+        System.out.println(g[1].toString());*/
      }
 }
